8362207a4a77f33ca9b6f0415ba0ca6947b1bb14
Mahak Agarwal <mahakagarwal45@gmail.com>
2026-03-30 17:30:21 +0530

    backend deploy

--- src/main/java/com/capstone/project/taskmanager/security/SecurityConfig.java
+++ src/main/java/com/capstone/project/taskmanager/security/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://taskmanager-frontend-aws.s3-website-ap-south-1.amazonaws.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
